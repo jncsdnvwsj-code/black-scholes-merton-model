a52baaaa1fd92d9e7c4e8cca45c283b5a29a6294
Léo Fabrègues <jncsdnvwsj@privaterelay.appleid.com>
2025-12-05 21:07:55 +0100

    Create BSM model and greeks.py

---
+++ BSM model and greeks.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+K = float(input("Enter the value of the strike price of the European call/put option"))
+T = float(input("Enter the maturity in years of the European call/put option"))
+sigma = float(input("Enter the volatility of the stock"))
+
